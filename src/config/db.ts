a432999d15a15f9b4ec34b8a4bae9ea28511a082
areumH <dkfmal0109@gmail.com>
2025-10-31 11:44:18 +0900

    chore: db 세팅 및 연결

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import mariadb from 'mariadb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  allowPublicKeyRetrieval: true, // Cloudtype 외부 접속용 옵션
+  ssl: false, // 외부 SSL 허용
+});
