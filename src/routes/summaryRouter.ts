9ff2ca9e8385cc9e81af4ed32c3effbbf28aeab2
Hwirin Kim <kimhuirin@gmail.com>
2025-11-01 22:35:03 +0900

    feat: 최근 글 3개 조회 api 완료

--- src/routes/summaryRouter.ts
+++ src/routes/summaryRouter.ts
@@ -1,16 +1,20 @@
-import { Router } from "express";
-import { Request, Response } from "express";
-import { getSummaryController } from "../controllers/summary.controller";
+import { Router } from 'express';
+import { Request, Response } from 'express';
+import {
+  getRecentSummaryController,
+  getSummaryController
+} from '../controllers/summary.controller';
 
 const summaryRouter = Router();
 
 summaryRouter.get(
-  "/",
+  '/',
   (req: Request, res: Response): Response<{ message: string }> => {
-    return res.status(200).json({ message: "Hello, World!" });
+    return res.status(200).json({ message: 'Hello, World!' });
   }
 );
 
-summaryRouter.get("/test", getSummaryController);
+summaryRouter.get('/test', getSummaryController);
+summaryRouter.get('/recent', getRecentSummaryController);
 
 export default summaryRouter;
