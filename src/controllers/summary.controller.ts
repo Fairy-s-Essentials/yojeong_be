7d11190cc1b0989c0923beb57136036a0b75ef03
Hwirin Kim <kimhuirin@gmail.com>
2025-11-03 21:30:48 +0900

    feat: main api 설계 완료

--- src/controllers/summary.controller.ts
+++ src/controllers/summary.controller.ts
@@ -24,26 +24,6 @@ export const getSummaryController = async (
   }
 };
 
-export const getRecentSummaryController = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log('getRecentSummaryController');
-  try {
-    //TODO: userId는 반드시 토큰에서 추출하는것으로 변경한다.
-    const userId = 1;
-    const summary = await getRecentSummary(userId);
-    return res.status(200).json({
-      success: true,
-      message: '최근 요약 조회 성공',
-      data: summary
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
 export const createSummaryController = async (
   req: Request<{}, {}, CreateSummaryReqBody>,
   res: Response,
