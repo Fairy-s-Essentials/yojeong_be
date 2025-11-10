83559103d8c398f91042d7596593773c02806363
Legitgoons <cksxkr5193@naver.com>
2025-11-10 19:48:18 +0900

    fix: history router 중복 제거

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -3,12 +3,10 @@ import {
   getMainAnalysisController,
   getMainRecentSummaryController,
 } from '../controllers/main.controller';
-import historyRouter from './historyRouter';
 
 const mainRouter = Router();
 
 mainRouter.get('/analysis', getMainAnalysisController);
 mainRouter.get('/recent-summary', getMainRecentSummaryController);
-mainRouter.use('/history', historyRouter);
 
 export default mainRouter;
